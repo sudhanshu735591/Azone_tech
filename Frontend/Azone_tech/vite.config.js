3856b9668162386ff6c5d9d1dceb3011422e8735
sudhanshu <ersshekhar05@gmail.com>
2025-06-30 13:31:43 +0530

    error resolution

--- Frontend/Azone_tech/vite.config.js
+++ Frontend/Azone_tech/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',                // Points to the root where index.html is
+  root: './index.html',                // Points to the root where index.html is
   build: {
     outDir: 'dist',         // Output folder for production build
     emptyOutDir: true,      // Clears the dist folder on each build
