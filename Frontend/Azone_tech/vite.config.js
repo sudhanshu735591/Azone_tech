0c63d725aa5f7172e1bbed24816b7ac468d38b43
sudhanshu <ersshekhar05@gmail.com>
2025-06-30 13:46:50 +0530

    updated code

--- Frontend/Azone_tech/vite.config.js
+++ Frontend/Azone_tech/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './index.html',                // Points to the root where index.html is
-  build: {
-    outDir: 'dist',         // Output folder for production build
-    emptyOutDir: true,      // Clears the dist folder on each build
-  },
 })
